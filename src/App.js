df21a085fbd1653cb8cbed1c680c1c35435af0ab
Lucia Brammer <brammerl96@gmail.com>
2020-04-27 17:28:14 -0700

    fixing typos

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Header/
+        <Header/>
         <Bar/>
         <Body/>
         <Recipe/>
